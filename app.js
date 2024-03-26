b6684181440ef48ae675f54173dd2f237298e46b
HiagoLima01 <hiago1234657@gmail.com>
2024-03-26 11:06:04 -0300

    Colando loop while

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 let num = 29;
-let chute = prompt('Escolha um número entre 1 e 30')
+let chute
 
-if (num == chute){
-    alert(`Parabéns você acertou o número secreto: ${num}`)
-}else{
-    if(num>chute){
-        alert(`O número secreto é menor que ${chute}`);
-    }
-    else{
-        alert(`O número secreto é maior que ${chute}`);
+while(chute != num){
+    chute = prompt("Escolha um número entre 1 e 10")
+    
+    if (num == chute){
+        alert(`Parabéns você acertou o número secreto: ${num}`)
+    }else{
+        if(num>chute){
+            alert(`O número secreto é menor que ${chute}`);
+        }
+        else{
+            alert(`O número secreto é maior que ${chute}`);
+        }
     }
 }
