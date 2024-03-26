6dac470b86f86a2a7fbf277a7c32bf86ea6f1ce4
HiagoLima01 <hiago1234657@gmail.com>
2024-03-26 11:00:13 -0300

    Melhorando lógica número maior e menor

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ let chute = prompt('Escolha um número entre 1 e 30')
 if (num == chute){
     alert(`Parabéns você acertou o número secreto: ${num}`)
 }else{
-    alert("Você errou!!")
+    if(num>chute){
+        alert(`O número secreto é menor que ${chute}`);
+    }
+    else{
+        alert(`O número secreto é maior que ${chute}`);
+    }
 }
