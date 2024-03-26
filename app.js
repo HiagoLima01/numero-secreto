73d75fe1f9527b0ac791ac584c6b452faa5311ff
Hiago Lima <hiago1234657@gmail.com>
2024-03-25 22:40:08 -0300

    Criando lógica if e else

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 let num = 29;
-prompt('Escolha um número entre 1 e 30')
+let chute = prompt('Escolha um número entre 1 e 30')
+
+if (num == chute){
+    alert("Parabéns você acertou o número secreto")
+}else{
+    alert("Você errou!!")
+}
