4a2865540e19d969f1774010a5f01f1c55d0f98b
Hiago Lima <hiago1234657@gmail.com>
2024-03-25 22:43:02 -0300

    Aprendendo sobre Template String

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let num = 29;
 let chute = prompt('Escolha um número entre 1 e 30')
 
 if (num == chute){
-    alert("Parabéns você acertou o número secreto")
+    alert(`Parabéns você acertou o número secreto: ${num}`)
 }else{
     alert("Você errou!!")
 }
