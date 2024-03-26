ededa2b5368376acdbb2fa5d6f396fcb3d7aed0f
HiagoLima01 <hiago1234657@gmail.com>
2024-03-26 11:11:23 -0300

    Adicionando soma de tentativas

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 let num = 29;
-let chute
+let chute;
+let tentativas = 1;
 
 while(chute != num){
     chute = prompt("Escolha um número entre 1 e 10")
-    
-    if (num == chute){
-        alert(`Parabéns você acertou o número secreto: ${num}`)
+
+    if (num === chute){
+        alert(`Parabéns você acertou o número secreto: ${num} com ${tentativas}`);
     }else{
         if(num>chute){
             alert(`O número secreto é menor que ${chute}`);
@@ -13,5 +14,7 @@ while(chute != num){
         else{
             alert(`O número secreto é maior que ${chute}`);
         }
+        tentativas += 1
     }
+
 }
